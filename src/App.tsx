dede207be2c7826ee07e197b36c6c5755f2d1c28
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-13 17:09:00 +0500

    pre-final functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
+import { useEffect, useState } from 'react'
+import { getPadTime } from './helpers/getPadTime'
+import './styles/app.scss'
+
 const App = () => {
-  return <div className="App"></div>
+  const [timeLeft, setTimeLeft] = useState(2 * 60)
+  const [isCounting, setIsCounting] = useState(false)
+
+  const minutes: any = getPadTime(Math.floor(timeLeft / 60))
+  const second = getPadTime(timeLeft - minutes * 60)
+
+  useEffect(() => {
+    const Interval = setInterval(() => {
+      isCounting && setTimeLeft((timeLeft) => (timeLeft >= 1 ? timeLeft - 1 : 0))
+    }, 1000)
+
+    if (timeLeft === 0) setIsCounting(false)
+
+    return () => {
+      clearInterval(Interval)
+    }
+  }, [timeLeft, isCounting])
+
+  const handleStart = () => {
+    if (timeLeft === 0) setTimeLeft(2 * 60)
+    setIsCounting(true)
+  }
+  const handleStop = () => {
+    setIsCounting(false)
+  }
+  const handleReset = () => {
+    setIsCounting(false)
+    setTimeLeft(2 * 60)
+  }
+
+  return (
+    <div className="app">
+      <div>
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{second}</span>
+      </div>
+      <div>
+        {isCounting ? (
+          <button onClick={handleStop}>Stop</button>
+        ) : (
+          <button onClick={handleStart}>Start</button>
+        )}
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  )
 }
 
 export default App
